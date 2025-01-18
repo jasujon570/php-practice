516d259b991ed99775836bdc69761a020f98f59f
jasujon570 <jasujon570@gmail.com>
2025-01-18 15:57:02 +0600

    Add php commenting file

---
+++ comments.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - Comments</title>
+</head>
+<body>
+    <h1>PHP Commenting</h1>
+    <?php
+        /* Comments are be used to:
+            1. Let other understand your code
+            2. Remind yourself of what you did
+            3. Leave out some parts of your code
+
+            Example:
+
+        Syntax for comments in PHP code:
+            // This is a single-line comment
+            # This is also a single-line comment
+
+            /* This is a 
+            multi-line comment */
+         
+        // outputs a welcome message;
+        echo "Welcome home!";
+        
+        echo "This is my first code"; #comment at the end of a line;
+
+        // Comments to Ignore Code
+        // echo "Happy coding!";
+    ?>;
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
