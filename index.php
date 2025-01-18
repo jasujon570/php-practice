a878a60bc26147e6ba92861e03a306426ac4afc9
jasujon570 <jasujon570@gmail.com>
2025-01-18 13:58:11 +0600

    Add about php case-sensitivity

--- index.php
+++ index.php
@@ -21,9 +21,33 @@
             //echo is the PHP build in function to output the result on webpage
 
             echo "Hello, I am sujon, currently practicing the PHP";
+
+            /* PHP Case Sensitivity
+            In php, keywords like "(if, else, while, echo, etc)", classes, fucntions, and user defined fucntions are not case-sensitive 
+            
+            But all kinds of variables are case-sensitive!
+
+
+            */
+            
+            #Example of case-sensitivity about "echo"
+
+            Echo "This is first code</br>";
+            ECHO "This is first code</br>";
+            eCHO "This is first code</br>";
             
 
+            #Example of case-sensitivity about variables
 
+            $age = 26;
+
+
+            echo "My age is " . $age . "<br>";
+            echo "My age is " . $AGE . "<br>";
+            echo "My age is " . $Age . "<br>";
+
+
+            // Only first statement will display the value of $age, rest of are treated as different variables
 
         ?>;
 
