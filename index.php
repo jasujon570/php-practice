425044e2dba93ce131869d8574407d208e6c65f8
jasujon570 <jasujon570@gmail.com>
2025-01-18 13:45:35 +0600

    File contains php syntax

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>PHP Syntax</H1>
+    <p> 
+        PHP - Hypertext Preprocessor </br>
+        
+        PHP file extension is ".php"</br>
+        
+        A PHP script starts with <?php
+        "and ends with" 
+        ?>; <!--PHP statement ends with semicolon (;)-->
+    </p>
+
+        <?php
+            //echo is the PHP build in function to output the result on webpage
+
+            echo "Hello, I am sujon, currently practicing the PHP";
+            
+
+
+
+        ?>;
+
+
+
+</body>
+</html>
