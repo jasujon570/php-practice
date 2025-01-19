0cc91d93e797d6ef34619b13a89f4e65f69c699f
jasujon570 <jasujon570@gmail.com>
2025-01-19 19:19:47 +0600

    Add file about PHP data types

---
+++ datatypes.php
@@ -0,0 +1,104 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Data Types</title>
+</head>
+<body>
+
+
+
+
+
+
+<?php
+//PHP Supports following data types
+
+/* 
+    - String
+    - Interger
+    - Float
+    - Boolean
+    - Array
+    - Object
+    - NULL
+    - Resource
+
+*/
+
+$myCountryName = "Bangladesh"; //php string
+
+
+echo "I love my country, my country name is {$myCountryName}. \n";
+
+var_dump($myCountryName);
+
+$x = 5684; //php integer
+$y = 9857;
+
+echo "$x/$y \n";
+var_dump($x);
+var_dump($y);
+
+
+$floatNumber = 15.5; //php float number
+var_dump($floatNumber);
+
+$bool = True; //PHP boolean
+$bool = False;
+var_dump($bool);
+var_dump($bool);
+
+
+$color = array("Red", "Green", "Yellow"); //PHP Array
+var_dump($color);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
