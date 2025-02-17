970167d20d12ef6f24d752c672f670220857069a
jasujon570 <jasujon570@gmail.com>
2025-02-17 19:10:38 +0600

    add 3 types of method

--- sumofevennumbers.php
+++ sumofevennumbers.php
@@ -1,14 +1,47 @@
-<?php
-//Create a PHP program that finds the sum of all even numbers between 1 and 100 using a loop
-
+<?php 
 $sum = 0;
 
-//loop through numbers from 1 to 100
-
-for($i = 2; $i <= 100; $i += 2) {
+for($i = 2; $i <= 100; $i += 2){
     $sum += $i;
 }
 
-echo "<p>The sum of all even numbers between 1 and 100 is: <b>$sum</b></p>";
+echo "<h3>The sum of all even numbers between 1 and 100 is : $sum</h3>";
+
+echo "/n";
+echo "/n";
+echo "/n";
+
+// using while loop
+
+
+$sum1 = 0;
+$x = 2;
+
+while($x <= 100){
+    $sum1 += $x;
+    $x += 2;
+}
+
+echo "<h4>The sum of all even numbers between 1 and 100 is : $sum1</h4>";
+
+echo "/n";
+echo "/n";
+echo "/n";
+
+// using array
+
+
+$sum3 = array_sum(range(2, 100, 2));
+echo "<h5>The sum of all even numbers between 1 and 100 is : $sum3</h5>"
+
+
+
+
+
+
+
+
+
+
 
 ?>
