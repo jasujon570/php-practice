20a16e8ff9854b52a16cfa57b32aef6b207239d5
jasujon570 <jasujon570@gmail.com>
2025-02-17 17:40:36 +0600

    Modify the file

--- vowelscount.php
+++ vowelscount.php
@@ -1,30 +1,22 @@
+
 <?php 
-//Write a function that takes a string and counts the number of vowels (a, e, i, o, u) in it.//
 
-function countVowels($str){
-    $vowels = ['a', 'e', 'i', 'o', 'u'];
-    $count = 0;
-
-    // Convert the string to lowercase to count both uppercase and lowercase vowels
-
-    $str = strtolower($str);
+    function countMyVowelsInString($str) {
+        $vowels = ['a', 'e', 'i', 'o', 'u'];
+        $count = 0;
 
 
-    // Loop through each character and check if it's a vowel
+        $str = strtolower($str);
 
-    for($i=0; $i < strlen($str); $i++){
-        if(in_array($str[$i], $vowels)){
-            $count++;
+        for($i=0; $i < strlen($str); $i++){
+            if(in_array($str[$i], $vowels)){
+                $count++;
+            }
         }
+        return $count;
     }
-    return $count;
 
-}
-
-echo countVowels("Welcome! I love php to learn and sharing my knowdlege");
-
-
-
+    echo countMyVowelsInString("keep patient! I am learning coding");
 
 
 
