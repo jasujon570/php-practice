b43d556aee32c3af6ae6de7cc54893ad89bf7d70
jasujon570 <jasujon570@gmail.com>
2025-02-17 20:30:16 +0600

    add some code

--- factorial_of_a_number.php
+++ factorial_of_a_number.php
@@ -12,7 +12,26 @@ function factorial($n){
     return $fact;
 }
 
-echo factorial(5);
+echo factorial(3);
+
+echo "\n";
+
+    echo PHP_EOL;
+    echo PHP_EOL;
+
+    function factorialRecursive($n) {
+        if($n < 0) {
+            return "Factorial is not defined for negative numbers";
+        }
+        if($n == 0 || $n == 1) {
+            return 1;
+        }
+        return $n * factorialRecursive($n - 1);
+    }
+
+
+    echo factorialRecursive(7);
+
 
 
 ?>
