40bb0439bbda136ea41145c1cafec849c6a53132
jasujon570 <jasujon570@gmail.com>
2025-02-28 21:56:54 +0600

    practicing basic php

--- basic-challanges.php
+++ basic-challanges.php
@@ -1,4 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simple Form Processing (POST)</title>
+</head>
+<body>
+    <form action="" method="POST">
+    Enter your Name: <input type="text" name="name" placeholder = "Enter your name" required>
+    Enter your Email: <input type="email" name="email" placeholder="Enter your email" required>
+    <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
+
+
 <?php
+
+/**
 
 function sum($num1, $num2){
     $sum = $num1 + $num2;
@@ -103,8 +122,27 @@ echo "\n";
         "Course" => "PHP Basics"
     ];
 
+?>
 
+**/
 
+ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Collect and sanitize inputs
+    $name = isset($_POST['name']) ? htmlspecialchars(trim($_POST['name'])) : "";
+    $email = isset($_POST['email']) ? filter_var(trim($_POST['email']), FILTER_SANITIZE_EMAIL) : "";
+    
+    // Check if both fields are provided
+
+    if(!empty($name) && !empty($email)) {
+        echo "Name: " . $name . "<br>";
+        echo "Email: " . $email . "<br>";
+    } else {
+        echo "Please fill in both fields";
+    }
+
+} else {
+    echo "Please submit the form";
+}
 
 
 
